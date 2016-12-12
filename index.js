363f769dfc0b002ceeb07e17ae37a5e99fac7676
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-12 13:06:25 +0100

    Pass "none" value to vector tile when gzip is not activated

--- index.js
+++ index.js
@@ -305,7 +305,7 @@ Bridge.getVector = function(source, map, z, x, y, callback) {
         if (vtile.empty()) {
             return callback(new Error('Tile does not exist'), null, headers);
         }
-        vtile.getData({ compression: source._gzip ? 'gzip' : undefined }, function(err, pbfz) {
+        vtile.getData({ compression: source._gzip ? 'gzip' : 'none' }, function(err, pbfz) {
             if (err) return callback(err);
             headers['Content-Encoding'] = source._gzip ? 'gzip' : undefined;
             if (source.BRIDGE_LOG_MAX_VTILE_BYTES_COMPRESSED > 0 && pbfz.length > source.BRIDGE_LOG_MAX_VTILE_BYTES_COMPRESSED) {
