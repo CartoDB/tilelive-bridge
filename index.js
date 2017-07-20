422b5f56ec6419da5aea25b0e8aa3014d14dd3b7
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-20 17:39:46 +0200

    Do not set encoding header if compression was not required

--- index.js
+++ index.js
@@ -316,8 +316,14 @@ Bridge.getVector = function(source, map, z, x, y, callback) {
             return callback(new Error('Tile does not exist'), null, headers);
         }
         vtile.getData({ compression: source._gzip ? 'gzip' : 'none' }, function(err, pbfz) {
-            if (err) return callback(err);
-            headers['Content-Encoding'] = source._gzip ? 'gzip' : undefined;
+            if (err) {
+                return callback(err);
+            }
+
+            if (source._gzip) {
+                headers['Content-Encoding'] = 'gzip';
+            }
+
             if (source.BRIDGE_LOG_MAX_VTILE_BYTES_COMPRESSED > 0 && pbfz.length > source.BRIDGE_LOG_MAX_VTILE_BYTES_COMPRESSED) {
                 stats.count++;
                 stats.total = stats.total + (pbfz.length*0.001);
