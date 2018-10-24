0feb30509619b4bfddad7e52d88703e6f72ce20d
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-10-24 14:49:40 +0200

    Avoid Syntax Error in strict mode: Delete of an unqualified identifier in strict mode

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ var ImagePool = function(size) {
         return callback(null,new mapnik.Image(size,size));
     }
     function destroy(im) {
-        delete im;
+        // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Delete_in_strict_mode
+        im = null;
     }
 }
 
